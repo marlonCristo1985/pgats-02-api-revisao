f97db0f29ddf20c290e697aafa75e5b1d072981e
marlonCristo1985 <marlon.developer@gmail.com>
2025-09-19 19:42:28 -0300

    adicionando testes http e corrigindo pipeline para rodar esses testes

---
+++ test/external/transferExternal.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const { expect } = require('chai')
+
+describe('Transfer External', () => {
+    describe('POST /transfers', () => {
+        it('Quando deixo de informar o remetente o retorno será 400 via HTTP', async () => {
+            const resposta = await request('http://localhost:3001')
+                .post('/api/transfers')
+                .send({
+                    remetente: "",
+                    destinatario: "jamile",
+                    valor: 600
+                });
+
+            expect(resposta.status).to.equal(400)
+            expect(resposta.body).to.have.property('message', 'Remetente, destinatario e valor são obrigatórios.')
+        })
+    })
+})
